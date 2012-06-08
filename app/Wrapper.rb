a55642f6afd4a85c278551353da42178a0e40cd7
Mark Haskamp <mark.haskamp@pinnsg.com>
2012-06-08 09:14:05 -0400

    word wrap kata

---
+++ app/Wrapper.rb
@@ -0,0 +1,55 @@
+# When recurring on a number, ask 1) 0? and 2) else
+# Always change at least one argument while recurring.
+# It must me changed to be closer to termination.
+# The changing argument must be tested in the termination condition.
+
+
+class Wrapper
+  def wrap str, max
+    final_str = ""
+
+    foo = get_first_split(str, max)
+    first_cut = foo[0]
+    remaining_str = foo[1]
+
+    while (remaining_str != "") do
+      final_str += first_cut + "\n"
+      foo = get_first_split(remaining_str, max)
+      first_cut = foo[0]
+      remaining_str = foo[1]
+    end
+    
+    final_str += first_cut
+  end
+
+  def get_first_split str, max
+
+    if str.length < max then
+      a = str
+      b = ""
+    else
+      ndx = get_split_index(str, max)
+      a = (str.slice(0, ndx)).chomp
+      b = str.slice(ndx, str.length).lstrip
+    end
+   
+    # puts "a,b: #{[a,b]}"
+    return [a,b]
+  end
+
+  def get_split_index str, max
+    return max if str[max] == ' '
+    ndx = 0
+    last_space_ndx = max
+    str.chars do |c|
+      last_space_ndx = ndx if c == ' '
+      
+      ndx += 1
+      return last_space_ndx if ndx == max
+    end
+
+    return last_space_ndx
+  end
+
+end
+
---
+++ spec/Wrapper.spec.rb
@@ -0,0 +1,54 @@
+require "~/dev/katas/word_wrap/app/Wrapper.rb"
+
+describe "Wrapper" do
+  w = Wrapper.new
+
+  describe "#wrap" do
+  
+    it "string less than max column, then returns string" do
+      w.wrap("foo", 80).should == "foo"
+    end
+
+    it "string longer than max, then splits the string at the max length" do
+      w.wrap("foobar", 3).should == "foo\nbar"
+    end
+
+    it "string must be split more than once" do
+      w.wrap("onetwothree", 3).should == "one\ntwo\nthr\nee"
+    end
+
+    it "string will be split on last space instead of at max length if appropriate" do
+      w.wrap("one two", 5).should == "one\ntwo"
+
+      w.wrap("one twothreefourfive six seven", 5).should ==
+              "one\ntwoth\nreefo\nurfiv\ne six\nseven"
+
+      w.wrap("12 456 890", 6).should == "12 456\n890"
+    end
+
+  end
+
+  describe "#get_split_index " do
+      it "returns max if there is no space" do
+        w.get_split_index("foo", 5).should == 5
+      end
+
+      it "returns index of last space when appropriate" do
+        w.get_split_index("foo bar",5).should == 3
+        w.get_split_index("foo bar baz", 8).should == 7
+      end
+
+      it "will return max if str[max] is a space" do
+        w.get_split_index("12 456 789",6).should == 6 
+      end
+
+  end
+
+end
+
+describe "rspec start" do
+  it "you shall not pass" do
+    false.should == false
+  end
+end
+
